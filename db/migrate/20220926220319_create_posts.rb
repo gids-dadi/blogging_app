45322641acae726a22b1e0255ba573a279945a54
gids-dadi <gideons564@gmail.com>
2022-09-27 01:48:37 +0100

    Correct linters

--- db/migrate/20220926220319_create_posts.rb
+++ db/migrate/20220926220319_create_posts.rb
@@ -1,11 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-        t.string :title
-        t.text :text
-        t.integer :commentsCounter
-        t.integer :likesCounter
-        
+      t.string :title
+      t.text :text
+      t.integer :commentsCounter
+      t.integer :likesCounter
+
       t.timestamps
     end
   end
