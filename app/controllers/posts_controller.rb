f927bf611c0cfb1a0e6f2b26c1d2414019e3195a
gids-dadi <gideons564@gmail.com>
2022-10-07 14:23:36 +0100

    Correct linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,12 @@
 class PostsController < ApplicationController
-
- def index
-@user = User.find(params[:user_id])
-@posts = Post.where(id: params[:user_id])
-    end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = Post.where(id: params[:user_id])
+  end
 
   def show
     @post = Post.find(params[:id])
-     @user = User.find(params[:user_id])
-     @comments = @post.comments
+    @user = User.find(params[:user_id])
+    @comments = @post.comments
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  
-resources :users, only: [ :index, :show ] do
-    resources :posts, only: [:index, :show]
+
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
   end
-     root  "users#index"
+  root 'users#index'
 end
