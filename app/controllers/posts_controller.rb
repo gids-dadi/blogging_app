826def82dfe5bb94ca5737a0b5c7690a95e80703
gids-dadi <gideons564@gmail.com>
2022-10-07 13:42:22 +0100

    Add the view endpoints

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,13 @@
 class PostsController < ApplicationController
-  def index
-    @user = User.find(params[:user_id])
-    @posts = Post.includes(:user).where(user_id: params[:user_id]).references(:posts)
-  end
+
+ def index
+@user = User.find(params[:user_id])
+@posts = Post.where(id: params[:user_id])
+    end
 
   def show
     @post = Post.find(params[:id])
-    @user = @post.user
+     @user = User.find(params[:user_id])
+     @comments = @post.comments
   end
 end
