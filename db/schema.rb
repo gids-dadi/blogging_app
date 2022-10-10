4844c1cc5ddc842e593e238a7a3f4fcd89014dbb
gids-dadi <gideons564@gmail.com>
2022-10-10 14:10:33 +0100

    Follow rails best practices execise

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_08_144153) do
     t.text "text"
     t.integer "commentsCounter"
     t.integer "likesCounter"
-    t.datetime "created_at", null: false
+    t.datetime "created_at", null: false 
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
