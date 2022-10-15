5fe494df286916b3fdae9d9b8ba38b3d9bb0eb6c
gids-dadi <gideons564@gmail.com>
2022-10-15 13:38:06 +0100

    Correct linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,6 @@ class PostsController < ApplicationController
   def create
     post = Post.new(post_params)
     post.user = current_user
-    
 
     if post.save
       flash[:success] = 'Post saved successfully'
